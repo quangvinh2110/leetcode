9ae1c80d1c72445136b52802cdc1ab212edcefc2
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2024-06-02 00:32:43 +0700

    Time: 180 ms (48.57%), Space: 12.9 MB (57.17%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,31 +1,12 @@
-class Solution(object):
-    
-    def binary_search(self, start_id, nums, target):
-        # print("start_id", start_id)
-        # print("nums: ", nums)
-        if len(nums)==0:
-            return -1
-        if len(nums)==1:
-            if nums[0]==target:
-                return start_id
-            else:
-                return -1
-            
-        median_id = len(nums)//2
-        median = nums[median_id]
-        if target > median:
-            return self.binary_search(start_id+median_id+1, nums[median_id+1:], target)
-        elif target < median:
-            return self.binary_search(start_id, nums[:median_id], target)
-        else:
-            return start_id + median_id
-        
-        
+class Solution:
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        return self.binary_search(0, nums, target)
-        
+        left, right = 0, len(nums)-1
+        while left<=right:
+            mid = (right+left)//2
+            if nums[mid] == target:
+                return mid
+            elif target>nums[mid]:
+                left = mid+1
+            else:
+                right = mid-1
+        return -1 
