29fd3260db7a620005bff0331e650d281244d02f
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2024-06-03 14:36:17 +0700

    Time: 48 ms (56.74%), Space: 13.1 MB (6.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,28 +1,4 @@
-class Node:
-    def __init__(self, value, ind):
-        self.value = value
-        self.ind = ind
-
 class Solution(object):
-
-    def binary_search(self, start_id, sorted_list, target):
-        if len(sorted_list)==0:
-            return -1
-        if len(sorted_list)==1:
-            if target == sorted_list[0].value:
-                return start_id
-            else:
-                return -1
-
-        mid_id = len(sorted_list)//2
-        median = sorted_list[mid_id].value
-        if target == median:
-            return start_id + mid_id
-        elif target > median:
-            return self.binary_search(start_id+mid_id+1, sorted_list[mid_id+1:], target)
-        else:
-            return self.binary_search(start_id, sorted_list[:mid_id], target)
-        
 
 
     def twoSum(self, nums, target):
@@ -31,11 +7,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        nums = [Node(num, i) for i, num in enumerate(nums)]
-        nums.sort(key=lambda x: x.value)
-        for i, num in enumerate(nums):
-            j = self.binary_search(0, nums[:i] + nums[i+1:], target-nums[i].value)
-            if j!=-1:
-                if j>=i:
-                    j=j+1
-                return [nums[i].ind, nums[j].ind]
+
+        nums = [(num, i) for i, num in enumerate(nums)]
+        nums.sort(key=lambda x: x[0])
+        
+        first = 0
+        last = len(nums)-1
+        while first<last:
+            if nums[first][0]+nums[last][0]>target:
+                last-=1
+            elif nums[first][0]+nums[last][0]<target:
+                first+=1
+            else:
+                return [nums[first][1], nums[last][1]]
+            
