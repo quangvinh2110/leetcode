16728360126814fd38e1f674d634a66e986d5c42
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2025-01-03 11:08:41 +0700

    Time: 4 ms (63.93%), Space: 18.4 MB (42.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # non zero index
-        nzi=0
+        non_0_ind = 0
         for i in range(len(nums)):
             if nums[i]!=0:
-                nums[nzi],nums[i]=nums[i],nums[nzi]
-                nzi=nzi+1
+                nums[i], nums[non_0_ind] = nums[non_0_ind], nums[i]
+                non_0_ind+=1
+        return nums
