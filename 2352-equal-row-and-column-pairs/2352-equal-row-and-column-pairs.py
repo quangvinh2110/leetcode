17c4f898b0447123248e28b556003f1e37c323b3
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2025-01-09 10:44:51 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,7 +1,7 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         from collections import Counter
-        hashmap = Counter([','.join(str(x) for x in row) for row in grid])
+        hashmap = Counter([' '.join(str(x) for x in row) for row in grid])
         grid_T = list(zip(*grid))
         count = 0
         for row in grid_T:
