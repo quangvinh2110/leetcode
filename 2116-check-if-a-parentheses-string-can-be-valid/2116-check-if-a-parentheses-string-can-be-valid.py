131d86e7648b273f2a2556850982db39794ff343
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2025-01-16 15:10:18 +0700

    Time: 49 ms (98.68%), Space: 21.4 MB (39.17%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
@@ -1,58 +1,31 @@
 class Solution:
-    @classmethod
     def canBeValid(self, s: str, locked: str) -> bool:
-        if len(s)%2==1:
+        length = len(s)
+
+        if length % 2 == 1 or (s[0] == ')' and locked[0] == '1') or (s[-1] == '(' and locked[-1] == '1'):
             return False
-        if s[0]==")" and locked[0]=="1":
-            return False
 
-        stack = []
-        stack_ind = []
-        unlocked_left = []
-        unlocked_right = []
+        open_brackets = []
+        unlocked = []
 
-        for ind, (char, flag) in enumerate(zip(s, locked)):
-            if flag=="0":
-                if char==")":
-                    unlocked_right.append(char)
-                
-            if len(stack)>0 and stack[-1]=="(" and char==")":
-                stack.pop()
-                stack_ind.pop()
-            elif len(stack)==0 and char==")":
-                if len(unlocked_right)>0:
-                    stack.append("(")
-                    stack_ind.append(ind)
-                    unlocked_right.pop()
-                else:
-                    print("hfhlsjfhsl")
-                    print(unlocked_right)
-                    return False
-            else:
-                stack.append(char)
-                stack_ind.append(ind)
-
-        if not stack:
-            return True
-
-        for ind, (char, flag) in reversed(list(enumerate(zip(s, locked)))):
-            if flag=="0":
-                if char=="(":
-                    unlocked_left.append(char)
-            if len(stack_ind)==0:
-                return True
-            if ind==stack_ind[-1]:
-                if len(unlocked_left)>0:
-                    stack.pop()
-                    stack.pop()
-                    stack_ind.pop()
-                    stack_ind.pop()
-                    unlocked_left.pop()
+        for i in range(length):
+            if locked[i] == "0":
+                unlocked.append(i)
+            elif s[i] == "(":
+                open_brackets.append(i)
+            elif s[i] == ")":
+                if open_brackets:
+                    open_brackets.pop()
+                elif unlocked:
+                    unlocked.pop()
                 else:
                     return False
+        
+        while open_brackets and unlocked and open_brackets[-1] < unlocked[-1]:
+            open_brackets.pop()
+            unlocked.pop()
 
-        # print(stack)
-        # print(stack_ind)
-        if not stack:
-            return True
-        return False
+        if open_brackets:
+            return False
+
+        return True
