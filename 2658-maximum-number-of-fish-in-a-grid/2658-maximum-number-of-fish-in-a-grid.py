64948f68560f0e86d20ce89fd30988256653cb27
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2025-01-28 01:14:47 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
@@ -4,28 +4,29 @@ class Solution:
         #     return 0
         m = len(grid)
         n = len(grid[0])
-        ind_set = {(i, j) for i in range(m) for j in range(n)}
+        ind_set = set()
         clusters = []
-        while len(ind_set)>0:
-            i, j = ind_set.pop()
-            n_fishs = 0
-            if grid[i][j]==0:
-                continue
-            bfs_queue = [(i,j)]
-            while len(bfs_queue)>0:
-                i, j = bfs_queue.pop(0)
-                n_fishs+=grid[i][j]
-                if i<m-1 and grid[i+1][j]>0 and (i+1, j) in ind_set:
-                    ind_set.remove((i+1, j))
-                    bfs_queue.append((i+1, j))
-                if j<n-1 and grid[i][j+1]>0 and (i, j+1) in ind_set:
-                    ind_set.remove((i, j+1))
-                    bfs_queue.append((i, j+1))
-                if i>0 and grid[i-1][j]>0 and (i-1, j) in ind_set:
-                    ind_set.remove((i-1, j))
-                    bfs_queue.append((i-1, j))
-                if j>0 and grid[i][j-1]>0 and (i, j-1) in ind_set:
-                    ind_set.remove((i, j-1))
-                    bfs_queue.append((i, j-1))
-            clusters.append(n_fishs)
+        for i in range(m):
+            for j in range(n):
+                ind_set.add((i, j))
+                n_fishs = 0
+                if grid[i][j]==0:
+                    continue
+                bfs_queue = [(i,j)]
+                while len(bfs_queue)>0:
+                    i, j = bfs_queue.pop(0)
+                    n_fishs+=grid[i][j]
+                    if i<m-1 and grid[i+1][j]>0 and (i+1, j) not in ind_set:
+                        ind_set.add((i+1, j))
+                        bfs_queue.append((i+1, j))
+                    if j<n-1 and grid[i][j+1]>0 and (i, j+1) not in ind_set:
+                        ind_set.add((i, j+1))
+                        bfs_queue.append((i, j+1))
+                    if i>0 and grid[i-1][j]>0 and (i-1, j) not in ind_set:
+                        ind_set.add((i-1, j))
+                        bfs_queue.append((i-1, j))
+                    if j>0 and grid[i][j-1]>0 and (i, j-1) not in ind_set:
+                        ind_set.add((i, j-1))
+                        bfs_queue.append((i, j-1))
+                clusters.append(n_fishs)
         return max(clusters) if clusters else 0
